c45451aea36a7220eea04ba38066392a105506f5
Ken Eguchi <eguchi@haw.co.jp>
2016-04-19 19:38:13 +0900

    :new: モデル product を作成

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160418102017) do
+ActiveRecord::Schema.define(version: 20160419103556) do
+
+  create_table "products", force: :cascade do |t|
+    t.string   "name",       null: false
+    t.string   "link"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",            null: false
