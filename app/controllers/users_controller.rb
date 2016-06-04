d3b4d6a523ffdc7248a77e0325ba42eb3bd03625
Ken Eguchi <no.eggchicken@gmail.com>
2016-06-04 18:19:05 +0900

    name, code を表示・編集できるようにした

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -53,6 +53,6 @@ class UsersController < ApplicationController
   end
 
   def user_params
-    params.require(:user).permit(:email, :password, :password_confirmation)
+    params.require(:user).permit(:code, :name, :email, :password, :password_confirmation)
   end
 end
